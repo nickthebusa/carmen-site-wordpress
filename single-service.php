7aabcaf027340e7991eae6c83a8afdcb9594cedd
Nick Busa <nbusa1231@gmail.com>
2026-08-05 17:33:51 -0400

    changed single-service to have Services heading and the dropdown menu

--- single-service.php
+++ single-service.php
@@ -1,14 +1,25 @@
 <?php get_header(); ?>
 
 <main>
-
-    <?php
-    if (have_posts()) :
-      while (have_posts()) : the_post();
-        the_content();
-      endwhile;
-    endif;
-    ?>
+  <?php
+  echo render_block(array(
+    'blockName' => 'carmen/hero',
+    'attrs' => array(
+      'heading'      => 'Services',
+      'headingLevel' => 'h1',
+      'bgImageUrl'   => get_the_post_thumbnail_url(get_the_ID(), 'full'),
+      'bgImageId'    => get_post_thumbnail_id(get_the_ID()),
+    ),
+  ));
+  ?>
+  <?php echo render_block(array('blockName' => 'carmen/service-dropdown', 'attrs' => array())); ?>
+  <?php
+  if (have_posts()) :
+    while (have_posts()) : the_post();
+      the_content();
+    endwhile;
+  endif;
+  ?>
 
 </main>
 
