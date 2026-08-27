671ea8cb371489a06d16d3d7117a4e8446cf34a8
Nick Busa <nbusa1231@gmail.com>
2026-08-26 20:44:32 -0400

    change to not have header on single-service page

--- single-service.php
+++ single-service.php
@@ -1,17 +1,6 @@
 <?php get_header(); ?>
 
 <main>
-  <?php
-  echo render_block(array(
-    'blockName' => 'carmen/hero',
-    'attrs' => array(
-      'heading'      => 'Services',
-      'headingLevel' => 'h1',
-      'bgImageUrl'   => get_the_post_thumbnail_url(get_the_ID(), 'full'),
-      'bgImageId'    => get_post_thumbnail_id(get_the_ID()),
-    ),
-  ));
-  ?>
 
   <div class="page-content <?php echo esc_attr(get_page_content_class()); ?>">
     <?php
